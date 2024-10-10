7cb021b3ebc18816734f9bafd696153a9983518b
xingyin2024 <xingyin.thu@gmail.com>
2024-10-10 15:41:30 +0200

    add - create a Job Component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
 import "./App.css";
+import {Job} from "./Job"
 
 function App() {
   return (
     <div className="App">
       <Job salary={90000} position="Senior SDE" company="Amazon" />
       <Job salary={12000} position="Junior SDE" company="Google" />
-      <Job salary={10000} position="Project Manager" company="Netflix" />
+      <Job salary={13000} position="Project Manager" company="Netflix" />
     </div>
   );
 }
 
-const Job = (props) => {
-  return (
-    <div>
-      <h1> {props.salary}</h1>
-      <h1> {props.position}</h1>
-      <h1> {props.company}</h1>
-    </div>
-  );
-};
 
 export default App;
---
+++ src/Job.jsx
@@ -0,0 +1,11 @@
+export const Job = (props) => {
+  return (
+    <div>
+      <h1>{props.salary}</h1>
+      <h1>{props.position}</h1>
+      <h1>{props.company}</h1>
+      <hr />
+    </div>
+  );
+};
+
